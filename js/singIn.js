6197536d642761e7630bef68be4db1fddb2e328f
KenanBoyukkishiyev1997 <kanannb@outlook.com>
2020-07-29 12:30:53 +0400

    60% is done

---
+++ js/singIn.js
@@ -0,0 +1,4 @@
+$('.signIn').click(function(){
+    $( '.travelomnia__header__container__account__noauth' ).css( "display", "none" ),
+    $('.travelomnia__header__container__account__auth').css( "display", "flex" )
+  });
